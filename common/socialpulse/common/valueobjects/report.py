83594930ef38b88ad5c71a85d4bc4f879e27f31e
Julio Mistral <jmistral@google.com>
2025-03-14 22:11:54 +0000

    Add ReportParameters value object.
    Added the ReportParameters VO, which will be used across the services to
    hold info on a specific report's set up.
    
    Change-Id: I9de93fc84977ceaa84ad5bb3c54529e98baa32ad

---
+++ common/socialpulse/common/valueobjects/report.py
@@ -0,0 +1,72 @@
+"""Copyright 2025 Google LLC.
+
+Licensed under the Apache License, Version 2.0 (the "License");
+you may not use this file except in compliance with the License.
+You may obtain a copy of the License at
+
+ https://www.apache.org/licenses/LICENSE-2.0
+
+Unless required by applicable law or agreed to in writing, software
+distributed under the License is distributed on an "AS IS" BASIS,
+WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+See the License for the specific language governing permissions and
+limitations under the License.
+"""
+
+import dataclasses
+import datetime
+import enum
+
+
+class SocialContentSource(enum.Enum):
+  VIDEO_CONTENT = "video_content"
+  VIDEO_COMMENT = "video_comment"
+  X_POST = "x_thread"
+  REDDIT_POST = "reddit_post"
+  OTHER = "other"
+
+
+class AnalysisType(enum.Enum):
+  SENTIMENT_SCORE = "sentiment_score"
+  SENTIMENT_JUSTIFICATION = "justification"
+  SOURCE_COUNT = "source_count"
+
+
+class TopicType(enum.Enum):
+  BRAND = "brand"
+  PRODUCT = "product"
+  PRODUCT_FEATURE = "product_feature"
+  OTHER = "other"
+
+
+class TimeUnit(enum.Enum):
+  DAY = "day"
+  WEEK = "week"
+  MONTH = "month"
+
+
+@dataclasses.dataclass
+class Topic:
+  name: str
+  type: TopicType
+
+
+@dataclasses.dataclass
+class AnalysisWindow:
+  topics: list[Topic]
+  date_range_start: datetime.date
+  date_range_end: datetime.date
+
+
+@dataclasses.dataclass
+class AnalysisOutput:
+  type: AnalysisType
+  segment_timeframe: TimeUnit = TimeUnit.DAY
+
+
+@dataclasses.dataclass
+class ReportParameters:
+  event_date: datetime.date | None
+  sources: list[SocialContentSource]
+  analysis_windows: list[AnalysisWindow]
+  analysis_outputs: list[AnalysisOutput]
