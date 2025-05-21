05bade9a3f0e8cc83a2b62c1f8c99e52a0148c12
Djimbaye Andemel <djimbaye@google.com>
2025-05-21 15:24:42 -0700

    created prompt generation task for video and comment sentiment analysis
    Change-Id: I6d9ce59414410b3dccce048527cedcac6d827bae

--- services/analysis_service/src/tasks/text_prompt.py
+++ services/analysis_service/src/tasks/text_prompt.py
@@ -11,7 +11,7 @@
 #  WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 #  See the License for the specific language governing permissions and
 #  limitations under the License.
-"""Task to generate LLM prompts to analyze video content."""
+"""Task to generate LLM prompts to analyze text content."""
 import copy
 import logging
 import string
@@ -23,44 +23,52 @@ from tasks import core as tasks_core
 
 settings = config.Settings()
 
-VIDEO_EXTRACTION_SYSTEM_INSTRUCTION = """You are a video analyst that carefully
-    looks through all frames of provided videos, extracting out the pieces
-    necessary to respond to user prompts. Make sure to look through and listen
-    to the whole video, start to finish.  Only reference information in the
-    video itself in your response."""
+TEXT_EXTRACTION_SYSTEM_INSTRUCTION = """You are a text analyst that carefully
+    looks through all sentences of provided text, extracting out the pieces
+    necessary to respond to user prompts. Make sure to look through and read
+    the whole text sample, start to finish.  Only reference information in the
+    text itself in your response."""
 
 SENTIMENT_SCORE_PROMPT_TEMPLATE = """Ignoring any previous questions and
-    answers, examine the video and  identify if it contains any of the
-    following products or brands for you to analyze:
+    answers, examine the video comment and  identify if it references
+    any of the following products or brands for you to analyze:
 
     ${topic_list}
 
-    For each product or brand that you identify, analyze the video for
-    them and generate an overall sentiment score of the video towards
+    For each product or brand that you identify, analyze the video comment for
+    them and generate an overall sentiment score of the video comment towards
     that product or brand, where -1.0 represents an extremely negative
     sentiment, 1.0 represents an extremely positive sentiment, and 0 represents
     a neutral sentiment.
 
     In addition, generate a relevance score, that represents how revelevant the
-    video is towards the product or brand.  Where 0 means the
-    video doesn't mentiond the product or brand at all, and 100 means
-    the video is exclusively about the product or brand.
+    video comment is towards the product or brand.  Where 0 means the
+    video comment doesn't allude to product or brand at all, and 100 means
+    the video comment is exclusively about the product or brand.  For example,
+    if the comment is simply praising the video creator for creating a good
+    video (ie, "Great video! I really liked it!"), than the relevance score
+    should be 0.  If the comment specifically mentions the product or brand, or
+    if it mentions a feature of the product (ie, "I really like how you can
+    use the widget to search for X"), then it should have a relevance score of
+    100.
 
-    In addition, generate a short summary of 3 to 4 sentences that summarize the
-    video, hitting on what specific points the creator brings up about the
-    product or brand.
+    To help you analyze the video comment, here's a summary of the
+    video that the comments were posted to:
+
+    ${video_summary}
+
+    Finally, if for whatever reason you can't provide any analysis, like if
+    there's too little text in the comment, or a completely blank comment,
+    then return a response with a sentiment score of 0.0, a relevance score
+    of 0, and "N/A" for the product or brand.
+
+    Here's the video comment to analyze:
+
+    ${video_comment}
     """
 
-JUSTIFICATION_PROMPT_CLAUSE = """In addition, pull out up to 3 quotes that are
-    positive about the product or brand, and up to 3 quotes that are negative
-    about the product or brand.
 
-    Make sure to only include quotes from the video, and only quotes
-    about the products or brands you're analyzing.  Finally generate 1 row per
-    product or brand that you identify and analyze."""
-
-
-class GenerateLlmVideoAnalysisPrompts(tasks_core.SentimentTask):
+class GenerateLlmTextAnalysisPrompts(tasks_core.SentimentTask):
   """Luigi Task to generate prompts for the Llm model.
 
   This task will generate prompts based on some input data (e.g., a list of
@@ -69,12 +77,14 @@ class GenerateLlmVideoAnalysisPrompts(tasks_core.SentimentTask):
   """
 
   _INPUT_COLUMNS = [
+      "commentId",
       "videoId",
-      "videoTitle",
-      "videoDescription",
-      "channelId",
-      "channelTitle",
+      "authorId",
       "publishedAt",
+      "text",
+      "likeCount",
+      "numOfReplies",
+      "parentId",
   ]
 
   _FINAL_OUTPUT_COLUMNS = [
@@ -82,13 +92,13 @@ class GenerateLlmVideoAnalysisPrompts(tasks_core.SentimentTask):
   ]
 
   def _validate_data(self, data: pd.Dataframe):
-    """Function for validating the YouTube data Dataframe.
+    """Function for validating the YouTube comments dataframe.
 
-    This method validates the TouTube data dataframe by checking the
+    This method validates the YouTube comments dataframe by checking the
     presence of the required columns.
 
     Args:
-      data: A dataframe containing YouTube videos data.
+      data: A dataframe containing YouTube comments data.
 
     Returns:
       True if the validation is successful.
@@ -97,7 +107,7 @@ class GenerateLlmVideoAnalysisPrompts(tasks_core.SentimentTask):
     for column in self._INPUT_COLUMNS:
       if column not in data.columns:
         raise ValueError(
-            f"[{self.task_family}] videos dataFrame missing "
+            f"[{self.task_family}] comments dataFrame missing "
             f"'{column}' column."
         )
 
@@ -121,11 +131,11 @@ class GenerateLlmVideoAnalysisPrompts(tasks_core.SentimentTask):
       generated_prompts_df = input_target.load_sentiment_data()
       if _validate_data(generated_prompts_df):
         logging.info(
-            "[%s] Generating video Llm prompts...", self.task_family
+            "[%s] Generating text Llm prompts...", self.task_family
         )
 
         generated_prompts_df["prompt"] = (
-            generated_prompts_df.apply(self._construct_video_llm_prompt, axis=1)
+            generated_prompts_df.apply(self.construct_text_llm_prompt, axis=1)
         )
 
         # Ensure all final output columns are present
@@ -152,7 +162,7 @@ class GenerateLlmVideoAnalysisPrompts(tasks_core.SentimentTask):
       )
       raise
 
-  def _construct_video_llm_prompt(self, row: pd.Series) -> pd.Series:
+  def construct_text_llm_prompt(self, row: pd.Series) -> pd.Series:
     """Function for constructing a LLM prompt.
 
     This method generates a prompt for the LLM based on the content of the row.
@@ -170,7 +180,7 @@ class GenerateLlmVideoAnalysisPrompts(tasks_core.SentimentTask):
         ).with_prompt(
             self._generate_base_prompt(row)
         ).with_system_instruction(
-            VIDEO_EXTRACTION_SYSTEM_INSTRUCTION
+            TEXT_EXTRACTION_SYSTEM_INSTRUCTION
         ).with_response_schema(
             response_schema
         ).with_temperature(
@@ -180,23 +190,27 @@ class GenerateLlmVideoAnalysisPrompts(tasks_core.SentimentTask):
         )
     )
 
-    prompt_generator.with_file_data([
-        ("video/*", row["videoUrl"])
-    ])
-
     return pd.Series({
         "prompt": prompt_generator.build()
     })
 
-  def _generate_base_prompt(self) -> str:
+  def _generate_base_prompt(self, row: pd.Series) -> str:
     """Generates the base prompt for the LLM.
+
+    Args:
+      row: A row containing comment data.
 
     Returns:
       The base prompt for the LLM.
     """
+    yt_video_summary = row["videoSummary"]
+    yt_video_comment = row["text"]
+
     scoring_prompt = string.Template(SENTIMENT_SCORE_PROMPT_TEMPLATE)
     prompt = scoring_prompt.substitute(
-        topic_list=self.workflow_exec.topic
+        topic_list=self.workflow_exec.topic,
+        video_summary=yt_video_summary,
+        video_comment=yt_video_comment
     )
 
     return prompt
