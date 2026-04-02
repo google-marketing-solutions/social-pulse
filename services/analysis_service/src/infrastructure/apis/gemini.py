c47942a90821c2fe0acddc90f5eca7c79bfcccae
Julio A. Mistral <jmistral@google.com>
2026-04-02 08:58:20 -0700

    fix: Update Gemini model due to deprecation.
    Updated the Gemini API client implementation to use Gemini 3.1 pro
    preview due to the 3.0 preview being deprecated.
    
    Change-Id: Ia8c82782bdd844e470566fde97095c4529919831

--- services/analysis_service/src/infrastructure/apis/gemini.py
+++ services/analysis_service/src/infrastructure/apis/gemini.py
@@ -23,7 +23,8 @@ from tasks.ports import apis
 # Initialize logging
 logger = logging.getLogger(__name__)
 
-GEMINI_MODEL_NAME = "gemini-3-pro-preview"
+# TODO(jmistral): Add these as init parameters, instead of constants.
+GEMINI_MODEL_NAME = "gemini-3.1-pro-preview"
 GEMINI_MODEL_LOCATION = "global"
 
 
